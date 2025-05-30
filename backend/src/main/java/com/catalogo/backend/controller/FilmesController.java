8bbd162ea6ebfc82356eefd40ce14391ddf2b572
NicolleMP <nimendespoltosi46@gmail.com>
2025-05-30 16:44:15 -0300

    feat: Criação da lógica de controller, service e repository do catalogo de filmes

--- backend/src/main/java/com/catalogo/backend/controller/FilmesController.java
+++ backend/src/main/java/com/catalogo/backend/controller/FilmesController.java
@@ -1,4 +1,46 @@
 package com.catalogo.backend.controller;
 
+import com.catalogo.backend.model.Filmes;
+import com.catalogo.backend.service.FilmesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/filmes")
 public class FilmesController {
+
+    @Autowired
+    private FilmesService filmesService;
+
+    @GetMapping
+    public List<Filmes> listarFilmes() {
+        return filmesService.getAllFilmes();
+    }
+
+    @GetMapping("/genero/{genero}")
+    public List<Filmes> getFilmesByGenero(@PathVariable String genero) {
+        return filmesService.getFilmesByGenero(genero);
+    }
+
+    @GetMapping("/nome/{nome}")
+    public List<Filmes> getFilmesByNome(@PathVariable String nome) {
+        return filmesService.getFilmesByNome(nome);
+    }
+
+    @GetMapping("/{id}")
+    public Filmes getFilmeById(@PathVariable long id) {
+        return filmesService.getFilmeById(id);
+    }
+
+    @PostMapping
+    public Filmes createFilme(@RequestBody Filmes filme) {
+        return filmesService.saveFilme(filme);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteFilme(@PathVariable long id) {
+        filmesService.deleteFilme(id);
+    }
 }
--- backend/src/main/java/com/catalogo/backend/repository/FilmeRepository.java
+++ backend/src/main/java/com/catalogo/backend/repository/FilmeRepository.java
@@ -1,4 +1,11 @@
 package com.catalogo.backend.repository;
 
-public interface FilmeRepository {
+import com.catalogo.backend.model.Filmes;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface FilmeRepository extends JpaRepository<Filmes, Long> {
+    List<Filmes> findByNome(String nome);
+    List<Filmes> findByGenero(String genero);
 }
