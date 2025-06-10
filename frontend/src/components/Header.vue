c4d62519f193e66ed138ecae82c262f7d28c5b67
Patrick Prestes <patrickprestes977@gmail.com>
2025-06-10 14:47:33 -0300

    feat: created e styling do menu hamburguer

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -23,6 +23,13 @@
 </template>
 
 <script setup>
+import { ref } from 'vue'
+
+const isMenuOpen = ref(false)
+
+const toggleMenu = () => {
+  isMenuOpen.value = !isMenuOpen.value
+}
 
 </script>
 
@@ -143,6 +150,99 @@ a.movie-item:hover {
 
 .btn i {
   margin-right: 1rem;
+}
+
+.hamburger-menu {
+  --size: 30px;
+  height: var(--size);
+  width: var(--size);
+  cursor: pointer;
+  z-index: 101;
+  position: relative;
+  display: none;
+  align-items: center;
+}
+
+.hamburger {
+  position: relative;
+}
+
+.hamburger,
+.hamburger::before,
+.hamburger::after {
+  width: var(--size);
+  height: 3px;
+  border-radius: 0.5rem;
+  background-color: #ffffff;
+  transition: 0.4s;
+}
+
+.hamburger::before,
+.hamburger::after {
+  content: "";
+  position: absolute;
+  left: 0;
+}
+
+.hamburger::before {
+  top: -10px;
+}
+
+.hamburger::after {
+  bottom: -10px;
+}
+
+.hamburger-menu.active .hamburger {
+  background-color: transparent;
+}
+
+.hamburger-menu.active .hamburger::before {
+  transform-origin: top left;
+  transform: rotate(45deg);
+  left: 6px;
+}
+
+.hamburger-menu.active .hamburger::after {
+  transform-origin: bottom left;
+  transform: rotate(-45deg);
+  left: 6px;
+}
+
+
+@media screen and (max-width: 768px) {
+  .nav-menu {
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100vh;
+  width: 100vw;
+  background-color: rgba(0, 0, 0, 0.85);
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 30px;
+  transform: translateY(-100%);
+  transition: transform 0.3s ease-in-out, opacity 0.3s ease-in-out;
+  opacity: 0;
+  visibility: hidden;
+  z-index: 98;
+  }
+
+
+ .nav-menu.active {
+  transform: translateY(0);
+  opacity: 1;
+  visibility: visible;
+  }
+
+
+  .hamburger-menu {
+    display: flex;
+  }
+
+  .nav-menu li ~ li {
+    margin-left: 0;
+  }
 }
 
 
