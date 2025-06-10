05873be3d79cf30504213b671a599ee9e4abd6e1
Patrick Prestes <patrickprestes977@gmail.com>
2025-06-10 10:29:11 -0300

    config component Heade e page HomeMovie

--- frontend/src/views/HomeMovie.vue
+++ frontend/src/views/HomeMovie.vue
@@ -1,12 +1,10 @@
 <script setup>
-import { RouterView } from 'vue-router'
-
+import Header from '@/components/Header.vue' 
 </script>
 
 <template>
-  <RouterView />
+  <Header />
 </template>
 
 <style scoped>
-
 </style>
