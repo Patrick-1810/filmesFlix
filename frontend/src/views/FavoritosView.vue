5d97744ce85b38438b25d02ce7a5e2cdf8a5df86
NicolleMP <nimendespoltosi46@gmail.com>
2025-06-14 16:30:53 -0300

    feat: Favorites page with the logic of showing cards

---
+++ frontend/src/views/FavoritosView.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="layout">
+    <Header />
+
+    <div class="main-content">
+      <div class="favoritos-page">
+        <h1>Meus Favoritos ❤️</h1>
+        <div v-if="favoritos.length === 0">Nenhum filme favoritado.</div>
+        <div class="movie-grid">
+          <MovieCard
+            v-for="(filme, index) in favoritosFilmes"
+            :key="index"
+            :movie="filme"
+          />
+        </div>
+      </div>
+    </div>
+
+    <Footer />
+  </div>
+</template>
+
+<script setup>
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
+import MovieCard from "@/components/MovieCard.vue";
+import { ref, onMounted } from "vue";
+import api from "@/services/api";
+import favoritosApi from "@/services/favoritosApi";
+
+const favoritos = ref([]);
+const favoritosFilmes = ref([]);
+
+onMounted(async () => {
+  try {
+    const res = await favoritosApi.getFavoritos();
+    favoritos.value = res.data;
+
+    const filmesRes = await api.get("filmes");
+    favoritosFilmes.value = filmesRes.data.filter((filme) =>
+      favoritos.value.some((fav) => fav.filmeId === filme.id)
+    );
+  } catch (error) {
+    console.error("Erro ao carregar favoritos: ", error);
+  }
+});
+
+const carregarFavoritos = async () => {
+  try {
+    const res = await favoritosApi.getFavoritos();
+    favoritos.value = res.data;
+
+    const filmesRes = await api.get("filmes");
+    favoritosFilmes.value = filmesRes.data.filter((filme) =>
+      favoritos.value.some((fav) => fav.filmeId === filme.id)
+    );
+  } catch (error) {
+    console.error("Erro ao carregar favoritos: ", error);
+  }
+};
+
+onMounted(carregarFavoritos);
+</script>
+
+<style scoped>
+.layout {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+.main-content {
+  flex: 1;
+}
+
+.movie-grid {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+  justify-content: center;
+}
+
+.favoritos-page {
+  padding: 2rem 1rem;
+}
+</style>
