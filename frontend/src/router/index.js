8fbd77fd05d1dfe5ea083cdfe04ca646ed434463
NicolleMP <nimendespoltosi46@gmail.com>
2025-06-13 22:34:07 -0300

    adding movieDetail routes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeMovie from '@/views/HomeMovie.vue'
+import MovieDetail from '@/views/MovieDetail.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,7 +10,11 @@ const router = createRouter({
       name: 'home',
       component: HomeMovie,
     },
-    
+    {
+      path: '/filme/:id',
+      name: 'movieDetail',
+      component: MovieDetail
+    },
   ],
 })
 
